47cc24129a5457e08b9479b9c409617ce0c2bdf5
mrerealm <marco@e-realm.net>
2020-11-06 15:33:25 +1100

    Updated model

---
+++ TALCodeTest2020/Models/PremiumQuoteModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TALCodeTest2020.Models
+{
+    public class PremiumQuoteModel
+    {
+        public decimal Premium { get; set; }
+        public decimal Amount { get; set; }
+        public int OccupationRating { get; set; }
+        public string DOB { get; set; }
+        public int Age { get; set; }
+    }
+}
